ca5fda3013fdddc915a999d8889413f32f9c523d
pshatara <pshatara@gmail.com>
2014-08-15 15:23:32 -0700

    Working Solution for StringifyJSON
    Solution complete for all test cases.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,53 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+var testCounter = 1;
 var stringifyJSON = function(obj) {
-  // your code goes here
+  	// your code goes here
+  	var objType = typeof(obj);
+  	// Stringifies all string/number/booleon input
+	if (objType != 'object' || obj === null) {
+		if (objType == 'string') {
+			obj = '"'+obj+'"';
+		}
+		console.log(testCounter);
+		testCounter++;
+		return String(obj);
+
+	} else {
+		var json = [];
+		var arrTest = (obj && obj.constructor === Array);
+		// Stringifies input from arrays
+		if (arrTest) {
+			for (var i = 0; i < obj.length; i++) {
+				json.push(typeof(obj[i]) != 'object' ? (typeof(obj[i]) === 'string' ? '"' + String(obj[i]) + '"' : String(obj[i])) : stringifyJSON(obj[i]));
+			}
+			console.log(testCounter);
+			testCounter++;
+			return '[' + json + ']';
+		} else { //Strinifies input from all other objects including functions
+			for (var key in obj) {
+				var value = obj[key];
+				objType = typeof(value);
+
+				if (objType === 'string') {
+					value = '"' + value + '"';
+				} else if (objType === 'function') {
+					var x = value();
+					value = (x != 'object' ? x : stringifyJSON(x));
+				} else if (objType === 'object' && value !== null) {
+					value = stringifyJSON(value);
+				}
+
+				if (value !== undefined) {
+					json.push('"' + key + '"' + ':' + value);
+				} else if (value === null) {
+					json.push('"' + key + '"' + ':' + value);
+				}
+			}
+			console.log(testCounter);
+			testCounter++;
+			return ('{' + String(json) + '}');
+		}
+	}
 };
