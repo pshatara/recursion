c9e5e699a420434731878e02f3d5733d30f5a3a3
pshatara <pshatara@gmail.com>
2014-08-18 00:52:58 -0700

    Code in progress
    Attempts to loop through a tier of nodes, and when a node is found to
    have child nodes, recursively call the function on the next tier down.
    This recursive call is complete when no more child nodes are found.
    While navigating the DOM tree, any elements that contain the target
    class name are pushed up one tier until the function returns to it’s
    initial cycle, returning a complete list.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,28 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var currentNodeTier;
+  var list = [];
+
+  if (this === window) {
+  	currentNodeTier = document.body;
+  } else {
+  	currentNodeTier = this;
+  }
+
+  for (var i = 0; i < currentNodeTier.childNodes.length; i++) {
+  	if (currentNodeTier.childNodes[i].className !== undefined) {
+  	    if (currentNodeTier.childNodes[i].classList.contains(className)) {
+  	        list.push(currentNodeTier.childNodes[i].classList);
+  	    }
+  	}
+
+    if (currentNodeTier.childNodes[i].hasChildNodes()) {
+      console.log(currentNodeTier.childNodes[i]);
+      list.push(currentNodeTier.childNodes[i].getElementsByClassName(className));
+    }
+  }
+  
+  console.log(list);
+  return list;
 };
